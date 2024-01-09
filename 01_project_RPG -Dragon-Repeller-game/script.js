35105eebd03cafe84c09182bc10516577151d746
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-01-09 21:53:32 +0500

    project 01_project_RPG -Dragon-Repeller-game Step 29 completed completed

--- 01_project_RPG -Dragon-Repeller-game/script.js
+++ 01_project_RPG -Dragon-Repeller-game/script.js
@@ -3,4 +3,5 @@ let health = 100;
 let gold = 50;
 let currentWeapon = 0;
 let monsterHealth;
-let inventory = ["stick","dagger","sword"];
+// let inventory = ["stick","dagger","sword"];
+let inventory = ["stick"];
