7703937ad562095a6fe0d24e7e2fa881e452bee4
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-01-14 22:42:35 +0500

    project 01_project_RPG -Dragon-Repeller-game Step 32,33,34 are completed completed

--- 01_project_RPG -Dragon-Repeller-game/script.js
+++ 01_project_RPG -Dragon-Repeller-game/script.js
@@ -6,5 +6,14 @@ let monsterHealth;
 // let inventory = ["stick","dagger","sword"];
 let inventory = ["stick"];
 
-let button1 = document.querySelector("#button1");
-console.log(button1)
+const button1 = document.querySelector("#button1");
+const button3 = document.querySelector("#button3");
+const button2 = document.querySelector("#button2");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+
+// console.log(button1)
