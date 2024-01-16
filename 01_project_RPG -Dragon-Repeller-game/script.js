2be4ae8a72edc04b98e82bca18acc21e7f4b2221
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-01-16 10:43:13 +0500

    project 01_project_RPG -Dragon-Repeller-game Step 44 is completed

--- 01_project_RPG -Dragon-Repeller-game/script.js
+++ 01_project_RPG -Dragon-Repeller-game/script.js
@@ -19,7 +19,9 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 
 
 function goStore(){
- const result = button1.innerText = "Buy 10 health (10 gold)";
+  button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
 
 }
 
