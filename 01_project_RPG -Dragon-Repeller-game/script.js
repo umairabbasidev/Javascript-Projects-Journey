e71dff3fa8bf494f9686ea5288cdd20d6c0af3b7
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-01-17 11:07:32 +0500

    project 01_project_RPG -Dragon-Repeller-game Step 45 is completed

--- 01_project_RPG -Dragon-Repeller-game/script.js
+++ 01_project_RPG -Dragon-Repeller-game/script.js
@@ -17,25 +17,24 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
-
-function goStore(){
+function goStore() {
   button1.innerText = "Buy 10 health (10 gold)";
   button2.innerText = "Buy weapon (30 gold)";
   button3.innerText = "Go to town square";
-
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
 }
 
-
-function goCave () {
- console.log("Going to cave.");
+function goCave() {
+  console.log("Going to cave.");
 }
-
 
 function fightDragon() {
- console.log("Fighting dragon.");
+  console.log("Fighting dragon.");
 }
 
-// button1.addEventListener("click",goStore)
+
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
