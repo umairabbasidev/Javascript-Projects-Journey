26b255cf5e65b81f30d0de8fec62aab52a076a03
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-01-21 11:16:16 +0500

    project 01_project_RPG -Dragon-Repeller-game Step 82 is completed

--- 01_project_RPG -Dragon-Repeller-game/script.js
+++ 01_project_RPG -Dragon-Repeller-game/script.js
@@ -17,7 +17,12 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
-const weapons = []
+const weapons = [
+  { name: "stick", power: 5 },
+  { name: "dagger", power: 30 },
+  { name: "claw hammer", power: 50 },
+  { name: "sword", power: 100 },
+];
 
 const locations = [
   {
