d07561c53adc6d35daf3d274b472cb042d9b7313
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-02-05 13:15:36 +0500

    01_project_RPGDragonRepeller steps 69-79,are completed

--- 01_project_RPGDragonRepeller/app.js
+++ 01_project_RPGDragonRepeller/app.js
@@ -12,11 +12,30 @@ const button3 = document.querySelector("#button3");
 const text = document.querySelector("#text");
 const xpText = document.querySelector("#xpText");
 const healthText = document.querySelector("#healthText");
-const goldText = document.querySelector("#goldText");
+let goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const weapons = [
+  {
+    name: "stick",
+    power: 5,
+  },
+  {
+    name: "dagger",
+    power: 30,
+  },
+  {
+    name: "claw hammer",
+    power: 50,
+  },
+  {
+    name: "sword",
+    power: 100,
+  },
+];
+console.log(weapons[0].name);
 const locations = [
   {
     name: "town square",
@@ -34,8 +53,14 @@ const locations = [
     "button functions": [buyHealth, buyWeapon, goTown],
     text: "You enter the store.",
   },
+  {
+    name: "cave",
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    "button functions": [fightSlime, fightBeast, goTown],
+    text: "You enter the cave. You see some monsters.",
+  },
 ];
-// console.log(locations[1])
+
 // initialize buttons
 
 button1.onclick = goStore;
@@ -61,12 +86,34 @@ function goStore() {
 }
 
 function goCave() {
-  console.log("Going to cave.");
+  update(locations[2]);
 }
 
 function fightDragon() {
   console.log("Fighting dragon.");
 }
 
-function buyHealth() {}
-function buyWeapon() {}
+function buyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
+  }
+}
+function buyWeapon() {
+  if (gold >= 30) {
+    gold -= 30;
+    goldText.innerText = gold;
+    text.innerText =
+      "You now have a dagger. In your inventory you have: stick,dagger";
+  } else {
+    text.innerText = "You do not have enough gold to buy a weapon.";
+  }
+}
+
+function fightSlime() {}
+
+function fightBeast() {}
